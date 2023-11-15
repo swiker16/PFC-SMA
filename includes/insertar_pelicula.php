59cbaa5353298d41121a5d15f3b102deb685fe65
IKER\ikerf <ikerfrancos008@gmail.com>
2023-11-15 21:43:13 +0100

    dev

--- includes/insertar_pelicula.php
+++ includes/insertar_pelicula.php
@@ -3,6 +3,7 @@ try {
     $pdo = new PDO("mysql:host=172.20.0.3;dbname=magiccinema", "root", "root");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $titulo = $_POST["titulo"];
         $director = $_POST["director"];
@@ -14,23 +15,25 @@ try {
 
         
 
-        // Guardar la imagen en el servidor
-        $imagen_nombre = $_FILES["imagen"]["name"];
-        $imagen_temp = $_FILES["imagen"]["tmp_name"];
-        $ruta_imagen = "../assets/imagenes/" . $imagen_nombre;
-        move_uploaded_file($imagen_temp, $ruta_imagen);
+    // Guardar la imagen en el servidor
+    // Obtener el contenido binario del archivo
+    $imagen_binaria = file_get_contents($_FILES["imagen"]["tmp_name"]);
+
+    // Escapar el nombre del archivo para evitar inyección SQL
+    $nombre = $pdo->quote($_FILES["imagen"]["name"]);
+    $fecha = date("Y-m-d", strtotime($_POST["fecha"]));
 
         // Insertar en la base de datos
-        $stmt = $pdo->prepare("INSERT INTO peliculas (titulo, director, descripcion, genero, duracion, clasificacion, Fecha_de_estreno,imagen) 
+        $stmt = $pdo->prepare("INSERT INTO peliculas (titulo, director, descripcion, genero, duracion, clasificacion, fecha_de_estreno,imagen) 
         VALUES (:titulo, :director, :descripcion, :genero, :duracion, :clas, :fecha, :imagen)");
         $stmt->bindParam(':titulo', $titulo);
         $stmt->bindParam(':director', $director);
         $stmt->bindParam(':descripcion', $descripcion);
         $stmt->bindParam(':genero', $genero);
-        $stmt->bindParam(':duracion', $duracion);
+        $stmt->bindParam(':duracion', $duracion);   
         $stmt->bindParam(':clas', $clas);
         $stmt->bindParam(':fecha', $date);
-        $stmt->bindParam(':imagen', $ruta_imagen, PDO::PARAM_LOB);
+        $stmt->bindParam(':imagen', $imagen_binaria, PDO::PARAM_LOB);
         $stmt->execute();
 
         echo "Película insertada correctamente.";
--- index.php
+++ index.php
@@ -1,61 +1,143 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo de PHP y MySQL</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Magic Cinema</title>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Dropdown
+						</a>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li>
+								<hr class="dropdown-divider">
+							</li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+					</li>
+				</ul>
+				<form class="d-flex" role="search">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+
+		<?php
+		// Incluir el archivo de conexión
+		include_once 'includes/config.php';
+
+		$conexion = ConnectDatabase::conectar();
+
+		$sql = "SELECT titulo, imagen FROM carrusel";
+		$resultado = $conexion->query($sql);
+
+		if ($resultado->rowCount() > 0) {
+		?>
+			<div id="carouselExampleFade" class="carousel slide carousel-fade m-5">
+				<div class="carousel-inner">
+					<?php
+					$primero = true; // Variable para destacar la primera imagen
+					while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
+						$titulo = $registro['titulo'];
+						$imagen_binaria = $registro['imagen'];
+						$activeClass = $primero ? 'active' : ''; // Asignar la clase 'active' solo a la primera imagen
+					?>
+						<div class="carousel-item <?php echo $activeClass; ?>">
+							<?php echo '<img src="data:image/jpeg;base64,' . base64_encode($imagen_binaria) . '" alt="Imagen de la Película" class="d-block w-100">';
+							?>
+							<div class="carousel-caption d-none d-md-block text-left">
+								<h5 class="display-3"><?php echo $titulo; ?></h5>
+							</div>
+						</div>
+
+					<?php
+						$primero = false; // Después de la primera iteración, establecer la variable a false
+					}
+					?>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
+
+		<?php
+		}
+
+		?>
 
 <?php
-// Incluir el archivo de conexión
-include_once 'includes/config.php';
+// Obtener las últimas 4 imágenes y títulos de la tabla carrusel
+$sql = "SELECT * FROM peliculas ORDER BY pelicula_id DESC LIMIT 4";
+$resultado = $conexion->query($sql);
 
-$conexion = ConnectDatabase::conectar();
-
-$sql = "SELECT * from peliculas";
-
-$resultado = $conexion->prepare($sql);
-
-$resultado->execute();
-
-if ($resultado->rowCount() == 0) {
-    echo "<div class ='container'>";
-        echo "No hay ningúna publicacion subida.";
-    echo "</div>";
+if ($resultado->rowCount() > 0) {
+    ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <?php
+            while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                $imagen_binaria = $registro['imagen'];
+                $titulo = $registro['titulo'];
+                ?>
+                <div class="col-md-3 mb-4">
+                    <div class="card">
+                        <img src="data:image/jpeg;base64,<?php echo base64_encode($imagen_binaria); ?>" class="card-img-top" alt="<?php echo $titulo; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title text-center"><?php echo $titulo; ?></h5>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+    <?php
 } else {
-    while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
-        echo $registro['Titulo'];
-    }
+    echo "<p>No hay imágenes en la tabla carrusel.</p>";
 }
-
 ?>
-    <form action="includes/insertar_pelicula.php" method="post" enctype="multipart/form-data">
-        <label for="titulo">Título:</label>
-        <input type="text" name="titulo" required><br>
 
-        <label for="director">Director:</label>
-        <input type="text" name="director" required><br>
 
-        <label for="descripcion">descripcion:</label>
-        <input type="text" name="descripcion" required><br>
 
-        <label for="genero">genero:</label>
-        <input type="text" name="genero" required><br>
 
-        <label for="duracion">duracion:</label>
-        <input type="text" name="duracion" required><br>
-
-        <label for="clas">clas:</label>
-        <input type="text" name="clas" required><br>
-
-        <label for="fecha">datw:</label>
-        <input type="date" name="fecha" required><br>
-
-        <label for="imagen">Imagen:</label>
-        <input type="file" name="imagen" accept="image/*" required><br>
-
-        <input type="submit" value="Insertar Película">
-    </form>
+	</div>
 </body>
+
 </html>
