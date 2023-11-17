61edb68e8a7c76cae7699b92a192e84a258fa6c3
IKER\ikerf <ikerfrancos008@gmail.com>
2023-11-17 13:01:14 +0100

    dev

--- index.php
+++ index.php
@@ -2,142 +2,340 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600%7CUbuntu:300,400,500,700" rel="stylesheet">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="assets/css/nouislider.min.css">
+	<link rel="stylesheet" href="assets/css/ionicons.min.css">
+	<link rel="stylesheet" href="assets/css/plyr.css">
+	<link rel="stylesheet" href="assets/css/photoswipe.css">
+	<link rel="stylesheet" href="assets/css/default-skin.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="icon/favicon-32x32.png" sizes="32x32">
+	<link rel="apple-touch-icon" href="icon/favicon-32x32.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="icon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="icon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="icon/apple-touch-icon-144x144.png">
+
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="Dmitry Volkov">
 	<title>Magic Cinema</title>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+<body class="body">
+	
+	<!-- header -->
+	<header class="header">
+		<div class="header__wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__content">
+							<!-- header logo -->
+							<a href="index.php" class="header__logo">
+								<img src="assets/img/Magic_Cinema-removebg-preview.png" alt="">
+							</a>
+							<!-- end header logo -->
 
-<body>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-	<nav class="navbar navbar-expand-lg bg-body-tertiary">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Dropdown
-						</a>
-						<ul class="dropdown-menu">
-							<li><a class="dropdown-item" href="#">Action</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
-							<li>
-								<hr class="dropdown-divider">
-							</li>
-							<li><a class="dropdown-item" href="#">Something else here</a></li>
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" aria-disabled="true">Disabled</a>
-					</li>
-				</ul>
-				<form class="d-flex" role="search">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
-				</form>
+							<!-- header nav -->
+							<ul class="header__nav">
+								<!-- dropdown -->
+								<li class="header__nav-item">
+									<a class="dropdown-toggle header__nav-link" href="#" role="button" id="dropdownMenuHome" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Promociones</a>
+								</li>
+								<!-- end dropdown -->
+
+								<li class="header__nav-item">
+									<a href="pricing.html" class="header__nav-link">Experiencias</a>
+								</li>
+
+								<li class="header__nav-item">
+									<a href="faq.html" class="header__nav-link">Contactanos</a>
+								</li>
+								<!-- end dropdown -->
+							</ul>
+							<!-- end header nav -->
+
+							<!-- header auth -->
+							<div class="header__auth">
+								<a href="signin.html" class="header__sign-in">
+									<i class="icon ion-ios-log-in"></i>
+									<span>Iniciar Sesión</span>
+								</a>
+							</div>
+							<!-- end header auth -->
+
+							<!-- header menu btn -->
+							<button class="header__btn" type="button">
+								<span></span>
+								<span></span>
+								<span></span>
+							</button>
+							<!-- end header menu btn -->
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</nav>
-	<div class="container">
 
-		<?php
-		// Incluir el archivo de conexión
-		include_once 'includes/config.php';
+		<!-- header search -->
+		<form action="#" class="header__search">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__search-content">
+							<input type="text" placeholder="Search for a movie, TV Series that you are looking for">
 
-		$conexion = ConnectDatabase::conectar();
-
-		$sql = "SELECT titulo, imagen FROM carrusel";
-		$resultado = $conexion->query($sql);
-
-		if ($resultado->rowCount() > 0) {
-		?>
-			<div id="carouselExampleFade" class="carousel slide carousel-fade m-5">
-				<div class="carousel-inner">
-					<?php
-					$primero = true; // Variable para destacar la primera imagen
-					while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
-						$titulo = $registro['titulo'];
-						$imagen_binaria = $registro['imagen'];
-						$activeClass = $primero ? 'active' : ''; // Asignar la clase 'active' solo a la primera imagen
-					?>
-						<div class="carousel-item <?php echo $activeClass; ?>">
-							<?php echo '<img src="data:image/jpeg;base64,' . base64_encode($imagen_binaria) . '" alt="Imagen de la Película" class="d-block w-100">';
-							?>
-							<div class="carousel-caption d-none d-md-block text-left">
-								<h5 class="display-3"><?php echo $titulo; ?></h5>
-							</div>
+							<button type="button">search</button>
 						</div>
-
-					<?php
-						$primero = false; // Después de la primera iteración, establecer la variable a false
-					}
-					?>
+					</div>
 				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Previous</span>
-				</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
 			</div>
+		</form>
+		<!-- end header search -->
+	</header>
+	<?php
+include_once 'includes/config.php';
 
-		<?php
-		}
-
-		?>
-
-<?php
-// Obtener las últimas 4 imágenes y títulos de la tabla carrusel
-$sql = "SELECT * FROM peliculas ORDER BY pelicula_id DESC LIMIT 4";
+$conexion = ConnectDatabase::conectar();
+$sql = "SELECT * FROM peliculas ORDER BY pelicula_id DESC LIMIT 7";
 $resultado = $conexion->query($sql);
 
 if ($resultado->rowCount() > 0) {
-    ?>
+?>
+
+<!-- home -->
+<section class="home">
+    <!-- home bg -->
+    <div class="owl-carousel home__bg">
+        <div class="item home__cover" data-bg="assets/img/home/home__bg.jpg"></div>
+        <div class="item home__cover" data-bg="assets/img/home/home__bg2.jpg"></div>
+        <div class="item home__cover" data-bg="assets/img/home/home__bg3.jpg"></div>
+        <div class="item home__cover" data-bg="assets/img/home/home__bg4.jpg"></div>
+    </div>
+    <!-- end home bg -->
+
     <div class="container">
-        <div class="row justify-content-center">
-            <?php
-            while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
-                $imagen_binaria = $registro['imagen'];
-                $titulo = $registro['titulo'];
+        <div class="row">
+            <div class="col-12">
+                <h1 class="home__title"><b>TOP PELÍCULAS</b></h1>
+
+                <button class="home__nav home__nav--prev" type="button">
+                    <i class="icon ion-ios-arrow-round-back"></i>
+                </button>
+                <button class="home__nav home__nav--next" type="button">
+                    <i class="icon ion-ios-arrow-round-forward"></i>
+                </button>
+            </div>
+
+            <div class="col-12">
+                <div class="owl-carousel home__carousel">
+                    <?php
+                    while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                        $imagen_binaria = $registro['imagen'];
+                        $titulo = $registro['titulo'];
+                        $genero = $registro['genero'];
+                    ?>
+                        <div class="item">
+                            <!-- card -->
+                            <div class="card card--big">
+                                <div class="card__cover">
+                                    <img src="data:image/jpeg;base64,<?php echo base64_encode($imagen_binaria); ?>" class="card-img-top" alt="<?php echo $titulo; ?>">
+                                    <a href="#" class="card__play">
+                                        <i class="icon ion-ios-play"></i>
+                                    </a>
+                                </div>
+                                <div class="card__content">
+                                    <h3 class="card__title"><?php echo $titulo; ?></h3>
+                                    <span class="card__category">
+                                        <a href="#"><?php echo $genero; ?></a>
+                                    </span>
+                                </div>
+                            </div>
+                            <!-- end card -->
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+}
+?>
+
+	<!-- end home -->
+
+	<!-- content -->
+	<section class="content">
+		<div class="content__head">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<!-- content title -->
+						<h2 class="content__title">Cartelera</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php
+include_once 'includes/config.php';
+
+$conexion = ConnectDatabase::conectar();
+$sql = "SELECT * FROM peliculas";
+$resultado = $conexion->query($sql);
+
+if ($resultado->rowCount() > 0) {
+?>
+
+<div class="container">
+    <!-- content tabs -->
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="tab-1" role="tabpanel" aria-labelledby="1-tab">
+            <div class="row">
+                <?php
+                while ($registro = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                    $imagen_binaria = $registro['imagen'];
+                    $titulo = $registro['titulo'];
+                    $genero = $registro['genero'];
+                    $edad = $registro['clasificacion'];
+                    $descripcion = $registro['descripcion'];
                 ?>
-                <div class="col-md-3 mb-4">
-                    <div class="card">
-                        <img src="data:image/jpeg;base64,<?php echo base64_encode($imagen_binaria); ?>" class="card-img-top" alt="<?php echo $titulo; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><?php echo $titulo; ?></h5>
+                <div class="col-6 col-sm-12 col-lg-6">
+                    <div class="card card--list">
+                        <div class="row">
+                            <div class="col-12 col-sm-4">
+                                <div class="card__cover">
+                                    <img src="data:image/jpeg;base64,<?php echo base64_encode($imagen_binaria); ?>" alt="<?php echo $titulo; ?>">
+                                    <a href="<?php echo $trailer_url; ?>" class="card__play">
+                                        <i class="icon ion-ios-play"></i>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-sm-8">
+                                <div class="card__content">
+                                    <h3 class="card__title"><a href="#"><?php echo $titulo; ?></a></h3>
+                                    <span class="card__category">
+                                        <a href="#"><?php echo $genero; ?></a>
+                                    </span>
+
+                                    <div class="card__wrap">
+
+                                        <ul class="card__list">
+                                            <li><?php echo $edad; ?></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="card__description">
+                                        <p><?php echo $descripcion; ?></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <?php
-            }
-            ?>
+                }
+                ?>
+            </div>
         </div>
     </div>
-    <?php
-} else {
-    echo "<p>No hay imágenes en la tabla carrusel.</p>";
+    <!-- end content tabs -->
+</div>
+
+<?php
 }
 ?>
 
+	</section>
+	<!-- end content -->
 
 
+	<!-- footer -->
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<!-- footer list -->
+				<div class="col-12 col-md-3">
+					<h6 class="footer__title">Download Our App</h6>
+					<ul class="footer__app">
+						<li><a href="#"><img src="img/Download_on_the_App_Store_Badge.svg" alt=""></a></li>
+						<li><a href="#"><img src="img/google-play-badge.png" alt=""></a></li>
+					</ul>
+				</div>
+				<!-- end footer list -->
 
-	</div>
+				<!-- footer list -->
+				<div class="col-6 col-sm-4 col-md-3">
+					<h6 class="footer__title">Resources</h6>
+					<ul class="footer__list">
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Pricing Plan</a></li>
+						<li><a href="#">Help</a></li>
+					</ul>
+				</div>
+				<!-- end footer list -->
+
+				<!-- footer list -->
+				<div class="col-6 col-sm-4 col-md-3">
+					<h6 class="footer__title">Legal</h6>
+					<ul class="footer__list">
+						<li><a href="#">Terms of Use</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Security</a></li>
+					</ul>
+				</div>
+				<!-- end footer list -->
+
+				<!-- footer list -->
+				<div class="col-12 col-sm-4 col-md-3">
+					<h6 class="footer__title">Contact</h6>
+					<ul class="footer__list">
+						<li><a href="tel:+18002345678">+1 (800) 234-5678</a></li>
+						<li><a href="mailto:support@moviego.com">support@flixgo.com</a></li>
+					</ul>
+					<ul class="footer__social">
+						<li class="facebook"><a href="#"><i class="icon ion-logo-facebook"></i></a></li>
+						<li class="instagram"><a href="#"><i class="icon ion-logo-instagram"></i></a></li>
+						<li class="twitter"><a href="#"><i class="icon ion-logo-twitter"></i></a></li>
+						<li class="vk"><a href="#"><i class="icon ion-logo-vk"></i></a></li>
+					</ul>
+				</div>
+				<!-- end footer list -->
+			</div>
+		</div>
+	</footer>
+	<!-- end footer -->
+
+	<!-- JS -->
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/assets/js/jquery.mousewheel.min.js"></script>
+	<script src="assets/js/jquery.mCustomScrollbar.min.js"></script>
+	<script src="assets/js/wNumb.js"></script>
+	<script src="assets/js/nouislider.min.js"></script>
+	<script src="assets/js/plyr.min.js"></script>
+	<script src="assets/js/jquery.morelines.min.js"></script>
+	<script src="assets/js/photoswipe.min.js"></script>
+	<script src="assets/js/photoswipe-ui-default.min.js"></script>
+	<script src="assets/js/main.js"></script>
 </body>
 
 </html>
